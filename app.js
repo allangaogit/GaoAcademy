236072c5928bbad443b80ed6538337ccf882d7ba
allan-gao-github <allangao94@gmail.com>
2020-06-25 23:51:10 -0400

    wrote html for lessons in unit1: alphabet, numbers,and basic expressions. Set up css files and images in a public folder, created a endpoint 'birthday' as a present for Sunny

--- app.js
+++ app.js
@@ -10,11 +10,17 @@ app.use(cors());
 app.use(express.json()); //let's us access req.body
 app.set("view engine", "ejs");
 app.use("/lessons", lessons);
+app.use( express.static( "public" ) );
+app.use( express.static( "views/partials" ));
 
 //routes
 
 app.get("/", (req, res) =>{
     res.render("index");
+});
+
+app.get("/birthday", (req, res) =>{
+    res.render("birthday");
 });
 
 
